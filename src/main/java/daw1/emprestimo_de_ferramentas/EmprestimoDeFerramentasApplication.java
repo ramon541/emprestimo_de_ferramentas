d7f62d54e03e2772deb9b041b8ac97800cdfb987
ramon541 <ramondiasmonteiro@gmail.com>
2024-11-18 20:36:54 -0300

    :tada: init: Start do projeto com Spring Boot

---
+++ src/main/java/daw1/emprestimo_de_ferramentas/EmprestimoDeFerramentasApplication.java
@@ -0,0 +1,13 @@
+package daw1.emprestimo_de_ferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoDeFerramentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoDeFerramentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daw1/emprestimo_de_ferramentas/EmprestimoDeFerramentasApplicationTests.java
@@ -0,0 +1,13 @@
+package daw1.emprestimo_de_ferramentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoDeFerramentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
