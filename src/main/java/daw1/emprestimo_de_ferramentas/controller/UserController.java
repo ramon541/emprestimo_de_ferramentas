5069cf16db168406086b4eaad37ca707e0d61464
ramon541 <ramondiasmonteiro@gmail.com>
2024-12-01 01:37:07 -0300

    :sparkles: feat: Layout da tela de login

--- src/main/java/daw1/emprestimo_de_ferramentas/controller/UserController.java
+++ src/main/java/daw1/emprestimo_de_ferramentas/controller/UserController.java
@@ -1,13 +1,20 @@
-package daw1.emprestimo_de_ferramentas.user;
+package daw1.emprestimo_de_ferramentas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/")
 public class UserController {
     
+    @GetMapping("/")
+    public String redirect() {
+        return "redirect:/login";
+    }
+    
+
     @GetMapping("/login")
     public String login() {
         return "user/login";
