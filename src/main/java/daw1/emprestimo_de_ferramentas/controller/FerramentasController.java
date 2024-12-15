4a1273d7d3c65a601d7a27fe4efaeeca43a849bb
Ramon Dias Monteiro Silva <122894193+ramon541@users.noreply.github.com>
2024-12-15 14:38:22 -0300

    :sparkles: feat: Crud de ferramentas (backend)

--- src/main/java/daw1/emprestimo_de_ferramentas/controller/FerramentasController.java
+++ src/main/java/daw1/emprestimo_de_ferramentas/controller/FerramentasController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,10 +31,34 @@ public class FerramentasController {
         this.ferramentaService = ferramentaService;
     }
 
+    //----------------------- Nova ferramenta -----------------------
+    @GetMapping("/get")
+    public List<FerramentaEntity> getAllFerramentas() {
+        return this.ferramentaService.getFerramentas();
+    }
+
+    //----------------------- Nova ferramenta -----------------------
     @PostMapping("/new")
     public ResponseEntity<Object> createFerramenta(@RequestBody FerramentaEntity ferramenta) {
         return ferramentaService.newFerramenta(ferramenta);
 
     }
 
+    //----------------------- Atualizar ferramenta -----------------------
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updateFerramentaById(@PathVariable Integer id, @RequestBody FerramentaEntity updatedFerramenta) {
+        return this.ferramentaService.updateFerramenta(id, updatedFerramenta);
+    }
+
+    //----------------------- Deletar ferramenta -----------------------
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> deleteFerramentaById(@PathVariable Integer id) {
+        return this.ferramentaService.deleteFerramenta(id);
+    }
+
+    //----------------------- Pegar ferramenta -----------------------
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Object> getFerramentaById(@PathVariable Integer id) {
+        return this.ferramentaService.getFerremantaById(id);
+    }
 }
--- src/main/java/daw1/emprestimo_de_ferramentas/services/FerramentaService.java
+++ src/main/java/daw1/emprestimo_de_ferramentas/services/FerramentaService.java
@@ -1,5 +1,8 @@
 package daw1.emprestimo_de_ferramentas.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,5 +27,47 @@ public class FerramentaService {
         return new ResponseEntity<>(ferramenta, HttpStatus.CREATED);
     }
     
-    
+    public List<FerramentaEntity> getFerramentas() {
+        return this.ferramentaRepository.findAll();
+    }
+
+    public ResponseEntity<Object> updateFerramenta(Integer id, FerramentaEntity updatedFerramenta) {
+        Optional<FerramentaEntity> ferramentaOptional = ferramentaRepository.findById(id);
+
+        if (!ferramentaOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        FerramentaEntity existingFerramenta = ferramentaOptional.get();
+
+        existingFerramenta.setNome(updatedFerramenta.getNome());
+
+        ferramentaRepository.save(existingFerramenta);
+
+        return ResponseEntity.ok(existingFerramenta);
+    }
+
+    public ResponseEntity<Object> deleteFerramenta(Integer id) {
+        Optional<FerramentaEntity> ferramentaOptional = ferramentaRepository.findById(id);
+        
+        if (!ferramentaOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ferramentaRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<Object> getFerremantaById(Integer id) {
+        Optional<FerramentaEntity> ferramentaOptional = ferramentaRepository.findById(id);
+        
+        if (!ferramentaOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        FerramentaEntity ferramenta = ferramentaOptional.get();
+
+        return ResponseEntity.ok(ferramenta);
+    }
 }
